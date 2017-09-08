6e4f824389ca2d2e268ecfdb446ca97e9c883c21
canito487 <canito487@gmail.com>
2017-09-07 20:22:50 -0700

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,5 +1,5 @@
 # [Paths]
-# directorypath = /Users/alex.hernandez/Google Drive/Interactive Intelligence/GimpyExporterv1_2/TestCaseSets/
-# templatepath = /Users/alex.hernandez/Google Drive/Interactive Intelligence/GimpyExporterv1_2/add_a_test_case.json
-# excelpath = /Users/alex.hernandez/Google Drive/Interactive Intelligence/GimpyExporterv1_2/TestCaseTracker.xlsx
+# directorypath = 
+# templatepath = 
+# excelpath = 
 #
